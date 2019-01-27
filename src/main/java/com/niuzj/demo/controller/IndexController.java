89b047a2b24c9e248ed9557512c545e2d0356498
tom <1523892672@qq.com>
2019-01-27 16:24:56 +0800

    ok

--- src/main/java/com/niuzj/demo/controller/IndexController.java
+++ src/main/java/com/niuzj/demo/controller/IndexController.java
@@ -3,10 +3,10 @@ package com.niuzj.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @RestController
@@ -18,10 +18,25 @@ public class IndexController {
     private DiscoveryClient discoveryClient;
 
     @RequestMapping("/hello")
-    public String hello(String name){
+    public String hello(String name, @RequestHeader String airen, @RequestBody Map<String, Object> map){
         ServiceInstance localServiceInstance = discoveryClient.getLocalServiceInstance();
-        logger.info(name);
+        logger.info("name=" + name);
+        logger.info("map=" + map);
+        logger.info("airen=" + airen);
         logger.info("/hello, host=" + localServiceInstance.getHost() + ",serviceId=" + localServiceInstance.getServiceId());
+        return "http://www.baidu.com";
+    }
+
+    @RequestMapping("/uri")
+    public String uri(@RequestBody Map<String, Object> map, HttpServletResponse response){
+        logger.info("params=" + map);
+        response.addHeader("Location", "http://www.baidu.com?" + map.get("name"));
+        return "";
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    public String put(@PathVariable Integer id){
+        logger.info(id.toString());
         return "success";
     }
 
